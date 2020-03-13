c22bbcdf0e8aa5a13aa3393b1848ee08ed04e7dc
Andy Suarez <andysuarez36@gmail.com>
2020-03-13 00:19:53 -0700

    adding navbootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/layout/Dashboard";
+
 class App extends Component {
   render() {
     return (
--- src/components/layout/Dashboard.js
+++ src/components/layout/Dashboard.js
@@ -1,69 +1,67 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+
 class Dashboard extends Component {
   render() {
     return (
-        
-        <style type="text/css">
-        {`
-        .btn-flat {
-          background-color: purple;
-          color: white;
-        }
-    
-        .btn-xxl {
-          padding: 1rem 1.5rem;
-          font-size: 1.5rem;
-        }
-        `}
-      </style>
-    
-      <Button variant="flat" size="xxl">
-        flat button
-      </Button>
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
 
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
-        <div className="row">
-          <div className="col s12 center-align">
-            {/* <h4>
-              <b>Build</b> a login/auth app with the{" "}
-              <span style={{ fontFamily: "monospace" }}>MERN</span> stack from
-              scratch
-            </h4>
-            <p className="flow-text grey-text text-darken-1">
-              Create a (minimal) full-stack app with user authentication via
-              passport and JWTs
-            </p> */}
-            <br />
-            <div className="col s6">
-              <Link
-                to="/register"
-                style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px"
-                }}
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-              >
-                Register
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span>
+            <i className="fas fa-bars" style={{ color: '#fff' }} />
+          </span>
+        </button>
+  
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav m-auto">
+            <li className="nav-item active">
+              <Link className="nav-link text-white text-uppercase ml-5" to="/">
+                Home&nbsp;
+                <i className="fas fa-home" />
+                <span className="sr-only">(current)</span>
               </Link>
-            </div>
-            <div className="col s6">
+            </li>
+            <li className="nav-item">
               <Link
-                to="/login"
-                style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px"
-                }}
-                className="btn btn-large btn-flat waves-effect white black-text"
+                className="nav-link text-white text-uppercase ml-5"
+                to="/news"
               >
-                Log In
+                news
               </Link>
-            </div>
-          </div>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link text-white text-uppercase ml-5"
+                to="/contacts"
+              >
+                contact us
+              </Link>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <input
+              className="form-control mr-sm-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button
+              className="btn btn-outline-primary  my-2 my-sm-0"
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
         </div>
-      </div>
+      </nav>
     );
   }
 }
