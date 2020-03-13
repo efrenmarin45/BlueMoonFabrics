56fdd420d84487db15468d7d93758be23d094374
Andy Suarez <andysuarez36@gmail.com>
2020-03-12 23:50:18 -0700

    adding dummy layout to test

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Dashboard from "./components/layout/Dashboard";
 class App extends Component {
   render() {
     return (
@@ -13,6 +14,7 @@ class App extends Component {
           <Route exact path="/" component={Landing} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/dashboard" component={Dashboard} />
         </div>
       </Router>
     );
