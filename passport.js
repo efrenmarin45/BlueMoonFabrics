56eae11f6b71bfe22849f16631bd1d8e283c31cb
Efren_Marin <efren45marin@gmail.com>
2020-03-15 23:43:51 -0700

    Added passport file to allow the user to login.

---
+++ passport.js
@@ -0,0 +1,37 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+var User = require("./server/models/userLogin.js");
+
+passport.use(new LocalStrategy(
+  {
+    usernameField: "empID",
+    passwordField: 'password'
+  },
+  function (username, password, done) {
+    User.findOne({ 'empID': username }, function (err, user) {
+      if (err)
+        return done(err);
+
+      if (!user)
+        return done(null, {message: "User not Found"});
+
+      if (!user.validPassword(password))
+        return done(null, {message: "Invalid Password"}); 
+
+      return done(null, user);
+    });
+  }
+));
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+module.exports = passport;
